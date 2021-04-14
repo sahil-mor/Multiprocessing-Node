014077d73871998ca05fa499b3caf1270e726d18
Sahil Mor <iconsahil7@gmail.com>
2021-04-15 02:27:52 +0530

    adding console log and port corrected

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ var isPrime = require("./isPrimeFunc")
 
 app.get("/:num",(req,res) => {
     var {num} = req.params
+    console.log(`${num} comes for non-blocking which will fork route`)
     const childProcess = fork('./isPrime.js')
     childProcess.send({ num })
-    childProcess.on("message" , response => res.send(response) )
+    childProcess.on("message" , response => {
+        console.log(`${num} response will be sent for non-blocking which will fork route`)
+        res.send(response)
+    } )
 
 })
 
 app.get("/blocked/:num",(req,res)=>{
-
     var {num} = req.params
+    console.log(`${num} comes for blocking simple route`)
     const response = isPrime(num)
+    console.log(`${num} response will be sent for blocking simple route`)
     res.send(response)
 })
 
+var port = process.env.PORT || 3000
 
-app.listen(3000,() => {
-    console.log("at 3000")
+app.listen(port,() => {
+    console.log(`Server at ${port}`)
 })
 
 // 29355126551
